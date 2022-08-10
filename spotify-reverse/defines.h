e858bf3afc058568e3ba317cad605865eb3acd8b
Arsenii es3n1n <me@es3n.in>
2022-08-10 17:16:20 +0200

    bump version

--- spotify-reverse/defines.h
+++ spotify-reverse/defines.h
@@ -2,8 +2,8 @@
 
 // autoupdates
 
-// @note: es3n1n: v1.0.8 = 108
-#define UNSPOTIFY_VERSION 108
+// @note: es3n1n: v1.0.9 = 109
+#define UNSPOTIFY_VERSION 109
 
 #define AUTOUPDATER_DOMAIN "es3n.in"
 #define AUTOUPDATER_URL "unspotify.json"
